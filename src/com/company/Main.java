1e373199f6f3e6f89a7c09ce34989dec270bff7f
Mihai <mcci0023@scs.ubbcluj.ro>
2021-03-19 12:22:24 +0200

    Practice Lists Repo

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // O lista de stringuri
+
+        List<String> masini = new ArrayList<>();
+        // o lista inca re putem salva stringuri
+        masini.add("Dacia");
+        masini.add("VW");
+        masini.add("Tesla");
+        masini.add("Citroen");
+        masini.add("Audi");
+
+        /*1.cate caractere are fiecare string
+        * 2.lunigmea toatala caractere lista
+        * 3.lista inversata
+        * 4.primul si ultimul string din lista
+        * 5.toate Uppercase
+        * */
+        //4
+        //Afiseaza elem de pe pozitia zero, si masini.get(pozitie) din masini.size(lungimea listei
+        // -1, indexul ultimei intrari
+
+        System.out.println("Prima si ultima masina sunt: "
+                + masini.get(0)+" "+masini.get(masini.size()-1));
+        //3
+        System.out.println("Lista inversata");
+        for(int i=masini.size()-1; i>=0; i--){
+            System.out.println("Lungimea totala a stringului "+ masini.get(i).toUpperCase()
+                    + " este " +
+                    masini.get(i).length());
+        }
+
+    }
+}
